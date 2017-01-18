a16a261491f525198c9499ac80cce4049840f6f3
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-01-18 18:13:55 +0900

    SASAKI_TICKET-58 remove domain models temporarily.

--- apps/domain/model/OwnerGroup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var OwnerGroup;
-(function (OwnerGroup) {
-    OwnerGroup.ANONYMOUS = "ANONYMOUS";
-    OwnerGroup.ADMINISTRATOR = "ANONYMOUS";
-    OwnerGroup.MEMBER = "MEMBER";
-})(OwnerGroup || (OwnerGroup = {}));
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = OwnerGroup;
