b23c7f2a375ba949598c6873036d45ce4a022fd5
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-03-03 19:32:42 +0900

    set default port 8080

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const debug = createDebug('app:server');
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || process.env.npm_config_port);
+const port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
 /**
  * Create HTTP server.
