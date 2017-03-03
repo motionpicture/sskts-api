d975c61ca8b2b8de1774c9f6f780b4bcf1c7411e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-03-03 16:57:03 +0900

    add transactions-test

--- test/transactions-test.js
+++ test/transactions-test.js
@@ -1,5 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * transactionルーターテスト
  *
  * @ignore
  */
+const assert = require("assert");
+const HTTPStatus = require("http-status");
+const supertest = require("supertest");
+const app = require("../app/app");
+describe('GET /transaction/:id', () => {
+    it('not found', (done) => {
+        supertest(app)
+            .get('/transaction/0000000000')
+            .set('authorization', 'Bearer ' + process.env.SSKTS_API_ACCESS_TOKEN)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(HTTPStatus.NOT_FOUND)
+            .then((response) => {
+            assert.equal(response.body.data, null);
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+});
--- test/transactions-test.ts
+++ test/transactions-test.ts
@@ -3,3 +3,26 @@
  *
  * @ignore
  */
+import * as assert from 'assert';
+import * as HTTPStatus from 'http-status';
+import * as supertest from 'supertest';
+
+import * as app from '../app/app';
+
+describe('GET /transaction/:id', () => {
+    it('not found', (done) => {
+        supertest(app)
+            .get('/transaction/0000000000')
+            .set('authorization', 'Bearer ' + process.env.SSKTS_API_ACCESS_TOKEN)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(HTTPStatus.NOT_FOUND)
+            .then((response) => {
+                assert.equal(response.body.data, null);
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+    });
+});
+
