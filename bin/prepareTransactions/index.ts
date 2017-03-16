70bedfa59294fafd90fc9f4984fcaddb6796ef91
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-03-16 11:04:14 +0900

    SSKTS-139 取引準備ジョブを追加。

---
+++ bin/prepareTransactions/index.ts
@@ -0,0 +1,34 @@
+/**
+ * 開始準備のできた取引を用意するタスク
+ *
+ * @ignore
+ */
+import * as sskts from '@motionpicture/sskts-domain';
+import * as createDebug from 'debug';
+import * as mongoose from 'mongoose';
+
+const debug = createDebug('sskts-api:bin:prepareTransactions');
+const NUMBER_OF_TRANSACTIONS_PER_TASK = 300; // 1タスクで生成する取引数
+const EXPIRES_IN_SECONDS = 300; // 生成した取引がREADYステータスのままで期限切れするまでの時間
+
+async function main() {
+    debug('connecting mongodb...');
+    mongoose.connect(process.env.MONGOLAB_URI);
+
+    const transactionAdapter = sskts.adapter.transaction(mongoose.connection);
+    try {
+        debug('preparing transactions...');
+        await sskts.service.transaction.prepare(NUMBER_OF_TRANSACTIONS_PER_TASK, EXPIRES_IN_SECONDS)(transactionAdapter);
+    } catch (error) {
+        console.error(error);
+    }
+
+    mongoose.disconnect();
+}
+
+main().then(() => {
+    debug('success!');
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
