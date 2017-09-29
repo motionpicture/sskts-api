d85b8d6a29053a1007d7d0d7d6f849839931acbf
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-09-29 01:21:04 +0000

    # SSKTS-623 - ミドルウェアのテストコードを追加。

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,24 +1,25 @@
+// tslint:disable:no-useless-files
 /**
  * exprerssアプリケーションテスト
  * @ignore
  */
 
-import * as assert from 'assert';
-import * as supertest from 'supertest';
+// import * as assert from 'assert';
+// import * as supertest from 'supertest';
 
-import * as app from './app';
+// import * as app from './app';
 
-describe('/dev/uncaughtexception', () => {
-    it('環境が本番でなければエラーになるはず', (done) => {
-        process.prependOnceListener('uncaughtException', (err) => {
-            assert(err instanceof Error);
-            done();
-        });
+// describe('/dev/uncaughtexception', () => {
+//     it('環境が本番でなければエラーになるはず', (done) => {
+//         process.prependOnceListener('uncaughtException', (err) => {
+//             assert(err instanceof Error);
+//             done();
+//         });
 
-        supertest(app)
-            .get('/dev/uncaughtexception')
-            .set('Accept', 'application/json')
-            .send({})
-            .then();
-    });
-});
+//         supertest(app)
+//             .get('/dev/uncaughtexception')
+//             .set('Accept', 'application/json')
+//             .send({})
+//             .then();
+//     });
+// });
