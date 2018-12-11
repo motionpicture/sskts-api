0752a4dd0cfe170690b9236b8db52b63de273d44
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-12-11 07:59:27 +0000

    # SSKTS-1091 ### Changed - 上映イベント検索条件強化

--- dst/app/routes/events.js
+++ dst/app/routes/events.js
@@ -1,9 +1,4 @@
 "use strict";
-/**
- * event router
- * イベントルーター
- * @module eventsRouter
- */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -13,9 +8,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * イベントルーター
+ */
 const sskts = require("@motionpicture/sskts-domain");
 const express_1 = require("express");
-const moment = require("moment");
 const redis = require("../../redis");
 const authentication_1 = require("../middlewares/authentication");
 const permitScopes_1 = require("../middlewares/permitScopes");
@@ -36,31 +33,24 @@ eventsRouter.get('/individualScreeningEvent/:identifier', permitScopes_1.default
     }
 }));
 eventsRouter.get('/individualScreeningEvent', permitScopes_1.default(['aws.cognito.signin.user.admin', 'events', 'events.read-only']), (req, __, next) => {
-    req.checkQuery('startFrom').optional().isISO8601().withMessage('startFrom must be ISO8601 timestamp');
-    req.checkQuery('startThrough').optional().isISO8601().withMessage('startThrough must be ISO8601 timestamp');
-    req.checkQuery('endFrom').optional().isISO8601().withMessage('endFrom must be ISO8601 timestamp');
-    req.checkQuery('endThrough').optional().isISO8601().withMessage('endThrough must be ISO8601 timestamp');
+    req.checkQuery('startFrom').optional().isISO8601().withMessage('startFrom must be ISO8601 timestamp').toDate();
+    req.checkQuery('startThrough').optional().isISO8601().withMessage('startThrough must be ISO8601 timestamp').toDate();
+    req.checkQuery('endFrom').optional().isISO8601().withMessage('endFrom must be ISO8601 timestamp').toDate();
+    req.checkQuery('endThrough').optional().isISO8601().withMessage('endThrough must be ISO8601 timestamp').toDate();
     next();
 }, validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     try {
-        // tslint:disable-next-line:no-suspicious-comment
-        // TODO 互換性維持のために<any>で一時対応
-        // dayとtheaterを削除する
-        const events = yield sskts.service.offer.searchIndividualScreeningEvents({
-            day: req.query.day,
-            theater: req.query.theater,
-            name: req.query.name,
-            startFrom: (req.query.startFrom !== undefined) ? moment(req.query.startFrom).toDate() : undefined,
-            startThrough: (req.query.startThrough !== undefined) ? moment(req.query.startThrough).toDate() : undefined,
-            endFrom: (req.query.endFrom !== undefined) ? moment(req.query.endFrom).toDate() : undefined,
-            endThrough: (req.query.endThrough !== undefined) ? moment(req.query.endThrough).toDate() : undefined,
-            eventStatuses: (Array.isArray(req.query.eventStatuses)) ? req.query.eventStatuses : undefined,
-            superEventLocationIdentifiers: (Array.isArray(req.query.superEventLocationIdentifiers)) ? req.query.superEventLocationIdentifiers : undefined,
-            workPerformedIdentifiers: (Array.isArray(req.query.workPerformedIdentifiers)) ? req.query.workPerformedIdentifiers : undefined
-        })({
-            event: new sskts.repository.Event(sskts.mongoose.connection),
-            itemAvailability: new sskts.repository.itemAvailability.IndividualScreeningEvent(redis.getClient())
+        const eventRepo = new sskts.repository.Event(sskts.mongoose.connection);
+        const itemAvailabilityRepo = new sskts.repository.itemAvailability.IndividualScreeningEvent(redis.getClient());
+        const searchConditions = Object.assign({}, req.query, { 
+            // tslint:disable-next-line:no-magic-numbers
+            limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : undefined, page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : undefined, sort: (req.query.sort !== undefined) ? req.query.sort : { startDate: sskts.factory.sortType.Ascending } });
+        const events = yield sskts.service.offer.searchIndividualScreeningEvents(searchConditions)({
+            event: eventRepo,
+            itemAvailability: itemAvailabilityRepo
         });
+        const totalCount = yield eventRepo.countIndividualScreeningEvents(searchConditions);
+        res.set('X-Total-Count', totalCount.toString());
         res.json(events);
     }
     catch (error) {
--- src/app/routes/events.ts
+++ src/app/routes/events.ts
@@ -1,12 +1,8 @@
 /**
- * event router
  * イベントルーター
- * @module eventsRouter
  */
-
 import * as sskts from '@motionpicture/sskts-domain';
 import { Router } from 'express';
-import * as moment from 'moment';
 
 import * as redis from '../../redis';
 import authentication from '../middlewares/authentication';
@@ -37,37 +33,33 @@ eventsRouter.get(
     '/individualScreeningEvent',
     permitScopes(['aws.cognito.signin.user.admin', 'events', 'events.read-only']),
     (req, __, next) => {
-        req.checkQuery('startFrom').optional().isISO8601().withMessage('startFrom must be ISO8601 timestamp');
-        req.checkQuery('startThrough').optional().isISO8601().withMessage('startThrough must be ISO8601 timestamp');
-        req.checkQuery('endFrom').optional().isISO8601().withMessage('endFrom must be ISO8601 timestamp');
-        req.checkQuery('endThrough').optional().isISO8601().withMessage('endThrough must be ISO8601 timestamp');
+        req.checkQuery('startFrom').optional().isISO8601().withMessage('startFrom must be ISO8601 timestamp').toDate();
+        req.checkQuery('startThrough').optional().isISO8601().withMessage('startThrough must be ISO8601 timestamp').toDate();
+        req.checkQuery('endFrom').optional().isISO8601().withMessage('endFrom must be ISO8601 timestamp').toDate();
+        req.checkQuery('endThrough').optional().isISO8601().withMessage('endThrough must be ISO8601 timestamp').toDate();
 
         next();
     },
     validator,
     async (req, res, next) => {
         try {
-            // tslint:disable-next-line:no-suspicious-comment
-            // TODO 互換性維持のために<any>で一時対応
-            // dayとtheaterを削除する
-            const events = await sskts.service.offer.searchIndividualScreeningEvents(<any>{
-                day: req.query.day,
-                theater: req.query.theater,
-                name: req.query.name,
-                startFrom: (req.query.startFrom !== undefined) ? moment(req.query.startFrom).toDate() : undefined,
-                startThrough: (req.query.startThrough !== undefined) ? moment(req.query.startThrough).toDate() : undefined,
-                endFrom: (req.query.endFrom !== undefined) ? moment(req.query.endFrom).toDate() : undefined,
-                endThrough: (req.query.endThrough !== undefined) ? moment(req.query.endThrough).toDate() : undefined,
-                eventStatuses: (Array.isArray(req.query.eventStatuses)) ? req.query.eventStatuses : undefined,
-                superEventLocationIdentifiers:
-                    (Array.isArray(req.query.superEventLocationIdentifiers)) ? req.query.superEventLocationIdentifiers : undefined,
-                workPerformedIdentifiers:
-                    (Array.isArray(req.query.workPerformedIdentifiers)) ? req.query.workPerformedIdentifiers : undefined
-            })({
-                event: new sskts.repository.Event(sskts.mongoose.connection),
-                itemAvailability: new sskts.repository.itemAvailability.IndividualScreeningEvent(redis.getClient())
-            });
+            const eventRepo = new sskts.repository.Event(sskts.mongoose.connection);
+            const itemAvailabilityRepo = new sskts.repository.itemAvailability.IndividualScreeningEvent(redis.getClient());
 
+            const searchConditions: sskts.factory.event.individualScreeningEvent.ISearchConditions = {
+                ...req.query,
+                // tslint:disable-next-line:no-magic-numbers
+                limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : undefined,
+                page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : undefined,
+                sort: (req.query.sort !== undefined) ? req.query.sort : { startDate: sskts.factory.sortType.Ascending }
+            };
+            const events = await sskts.service.offer.searchIndividualScreeningEvents(searchConditions)({
+                event: eventRepo,
+                itemAvailability: itemAvailabilityRepo
+            });
+            const totalCount = await eventRepo.countIndividualScreeningEvents(searchConditions);
+
+            res.set('X-Total-Count', totalCount.toString());
             res.json(events);
         } catch (error) {
             next(error);
